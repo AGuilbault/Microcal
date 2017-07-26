c8063793b8d7e354d02f95243ba883375ef29aad
joel-lemay <joel.lemay@usherbrooke.ca>
2017-07-26 11:21:35 -0400

    Added csv file recording and comments.

--- Microcal.py
+++ Microcal.py
@@ -61,43 +61,64 @@ class WidgetMain(QtWidgets.QWidget, WidgetMain.Ui_Form):
 
         # Create timer.
         self.timer = QtCore.QTimer()
+        # Set interval from the spin box.
         self.timer.setInterval(self.spin_interval.value())
-        self.spin_interval.valueChanged.connect(self.change_interval)
+        # If interval changed in spin box, update timer interval.
+        self.spin_interval.valueChanged.connect(self.timer.setInterval)
+        # At timeout, update pid and graph.
         self.timer.timeout.connect(self.wid_pid.update_pid)
-
         self.timer.timeout.connect(self.update_graph)
         self.timer.start()
 
         self.btn_browse.clicked.connect(self.browse)
         self.btn_record.clicked.connect(self.record)
 
+        # Init empty csv file variable.
+        self.csvfile = None
+
     def browse(self):
+        # Open file save dialog.
         filename, ext = QtWidgets.QFileDialog.getSaveFileName(self, 'Save file', 'C:\\', 'CSV files (*.csv)')
+        # If file selected.
         if filename != '':
+            # Update path in text box.
             self.edit_path.setText(filename)
 
     def record(self):
-        try:
-            csvfile = open(self.edit_path.text(), mode='w+t')
-            csvfile.write('Allo')
-            csvfile.close()
-            self.edit_path.setEnabled(False)
-            self.btn_record.setText('Stop')
-        except IOError as e:
-            QtWidgets.QMessageBox.critical(self, 'Error', e.strerror)
-
-    def change_interval(self):
-        self.timer.setInterval(self.spin_interval.value())
+        # If file is not open.
+        if self.csvfile is None or self.csvfile.closed:
+            try:
+                self.csvfile = open(self.edit_path.text(), mode='w+t')
+                self.edit_path.setEnabled(False)
+                self.btn_record.setText('Stop')
+                self.lbl_status.setText('Recording')
+            except IOError as e:
+                QtWidgets.QMessageBox.critical(self, 'Error', e.strerror)
+        # If file is open.
+        else:
+            # Close the file.
+            self.csvfile.close()
+            self.edit_path.setEnabled(True)
+            self.btn_record.setText('Save')
+            self.lbl_status.setText('Not recording')
 
     def update_graph(self):
+        # Append timestamp.
         self.data_x.append(self.elapsed.elapsed() / 1000)
+        # Append nVolt reading.
         self.data_y.append(self.wid_nvolt.fetch())
+        # Update plot.
         self.line_temp.set_data(self.data_x, self.data_y)
         self.ax.relim()
         self.ax.autoscale_view(True, True, True)
         self.ax.autoscale(enable=True)
         self.canvas.draw()
         self.toolbar.update()
+        # Append to csv file if open.
+        if self.csvfile is not None and not self.csvfile.closed:
+            self.csvfile.write('{0:.3f}, {1}\n'.format(self.data_x[-1], self.data_y[-1]))
+            self.csvfile.flush()
+            self.lbl_status.setText((self.lbl_status.text() + '.').replace('....', ''))
 
 
 if __name__ == "__main__":
@@ -112,9 +133,11 @@ if __name__ == "__main__":
     mainwind = QtWidgets.QMainWindow()
     mainwind.setCentralWidget(main.tab)
 
+    # Set window title.
     mainwind.setWindowTitle('Microcal station')
     mainwind.setMinimumSize(mainwind.minimumSizeHint())
 
+    # Show window.
     mainwind.show()
 
     # Run GUI loop.
